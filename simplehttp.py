258a034e80141694466ccddf04ff214628a751af
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-01 20:15:04 -0400

    Add root option, and missing import.

--- simplehttp.py
+++ simplehttp.py
@@ -4,20 +4,24 @@ import SocketServer
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-import sys, os
+import argparse, sys, os
 
 class ThreadingHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     pass
 
 def run():
+
     parser = argparse.ArgumentParser(prog="simplehttp")
     parser.add_argument('-i', '--interface', type=str, default="",
         help='Interface to connect to, defaults to all.')
     parser.add_argument('-p', '--port', type=int, default = 8080,
         help='Port to run on.')
+    parser.add_argument('-r', '--root', type=str, default="./",
+        help='Document root.  Defaults to current directory.')
 
     args = parser.parse_args(sys.argv[1:])
 
+    os.chdir(args.root)
     server_address = (args.interface, args.port)
     httpd = ThreadingHTTPServer(server_address, SimpleHTTPRequestHandler)
     print "Running on %s:%s" % server_address
