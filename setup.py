3e47d313f3a08b2bd77c492428a209510b206736
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-01 20:32:23 -0400

    Make installable.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='simplehttp',
+      version='1.0',
+      description='Super simple threaded web server',
+      author='Joshua D. Boyd',
+      author_email='jdboyd@jdboyd.net',
+      scripts=['simplehttp.py'],
+      entry_points = {'console_scripts': ['simplehttp = simplehttp:run']}
+     )
