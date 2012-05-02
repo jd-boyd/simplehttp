849cb7e23e29fa67c76a53f79e46bcb72c87608a
Joshua D. Boyd <jdboyd@jdboyd.net>
2012-05-01 20:12:24 -0400

    Put in a run function.

--- simplehttp.py
+++ simplehttp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import threading
 import SocketServer
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
@@ -10,13 +9,13 @@ import sys, os
 class ThreadingHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
     pass
 
+def run():
+    server_address = ('', 8080)
+    httpd = ThreadingHTTPServer(server_address, SimpleHTTPRequestHandler)
+    print "Running on %s:%s" % server_address
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print "Exiting on Ctrl-C."
 
-server_address = ('', 8080)
-httpd = ThreadingHTTPServer(server_address, SimpleHTTPRequestHandler)
-print "Running on %s:%s" % server_address
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    print "Exiting on Ctrl-C."
-
-
+run()
