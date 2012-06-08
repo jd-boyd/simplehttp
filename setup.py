bc730d79572084381dd41fecb4d28ed1288f57cc
Joshua Boyd <jdboyd@vs-networks.com>
2012-06-08 08:27:43 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='simplehttp',
-      version='1.0',
+      version='1.1',
       description='Super simple threaded web server',
       author='Joshua D. Boyd',
       author_email='jdboyd@jdboyd.net',
